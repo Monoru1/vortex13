595790c577724603b517eb67bb8744e873bbf3ad
Monoru1 <162912698+Monoru1@users.noreply.github.com>
2026-07-09 00:25:49 +0200

    feat(hero): isolate OGL chunk

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         manualChunks: {
           react: ["react", "react-dom", "react-router-dom"],
           motion: ["framer-motion"],
+          ogl: ["ogl"],
         },
       },
     },
